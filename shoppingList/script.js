a3ea3ca6dd4ae32c0e6c08086a416d34ec407903
Josefin H <josefin.hallsson@gmail.com>
2020-04-22 16:57:43 +0200

    tagit bort utfyllnadsitems. Brytit ut funktioner

--- shoppingList/script.js
+++ shoppingList/script.js
@@ -1,39 +1,45 @@
 let button = document.getElementById("addItem");
-let input = document.getElementById("listItem");
 
-
-button.addEventListener("click", function(){
-
-    //alert(input.value);
-    addToList(input);
+button.addEventListener("click", function() {
+  let input = document.getElementById("listItem");
+  addToList(input);
 });
 
-function addToList(item){
-  //shoppingList.innerHTML+="<div>"+input.value+"</div>";
+function addToList(item) {
   let shoppingList = document.getElementById("shoppingList");
+  let newPost = createAnchorTag(item);
+  //lägg till i listan
+  shoppingList.appendChild(newPost);
 
-  let newAnchor = document.createElement("a");                //skapa anchor-tag
-  newAnchor.setAttribute("class", "list-group-item list-group-item-action"); //ge klass
+}
 
-  let newButton=document.createElement("button");             //skapa button
-  newButton.setAttribute("type", "close");                    //ge attribut
+function createAnchorTag(item) {
+  //skapa anchor-tag
+  let newAnchor = document.createElement("a");
+  //ge klass
+  newAnchor.setAttribute("class", "list-group-item list-group-item-action");
+
+  //skapa text till inlägget
+  let itemText = document.createTextNode(item.value);
+  //lägg till text i anchor
+  newAnchor.appendChild(itemText);
+  //lägg till button i anchor
+  newAnchor.appendChild(createButtonTag());
+
+  return newAnchor;
+}
+
+function createButtonTag() {
+  //skapa button
+  let newButton = document.createElement("button");
+  //ge attribut
+  setButtonAttributes(newButton);
+  //lägg till krysset i button
+  newButton.appendChild(document.createTextNode("\u00D7"));
+  return newButton;
+}
+function setButtonAttributes(newButton){
+  newButton.setAttribute("type", "close");
   newButton.setAttribute("onclick", "alert('hej')");
   newButton.setAttribute("class", "close");
-  newButton.appendChild(document.createTextNode("\u00D7"));    //lägg till krysset i button
-
-  let itemText = document.createTextNode(item.value);       //skapa text till inlägget
-
-
-  newAnchor.appendChild(itemText);                         //lägg till text i anchor
-  newAnchor.appendChild(newButton);                         //lägg till button i anchor
-
-                                                            //sätt ihop inlägget
-  shoppingList.appendChild(newAnchor);                        //lägg till i listan
-
-}
-function createAnchorTag(){
-
-}
-function createButtonTag(){
-  
 }
