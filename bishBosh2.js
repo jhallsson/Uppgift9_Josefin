2016372bdee4f05f315b7c4deec5cb19ea8aa8c4
Josefin H <josefin.hallsson@gmail.com>
2020-04-22 14:36:18 +0200

    fixat ui. skrivit över bishbosh2

--- bishBosh2.js
+++ bishBosh2.js
@@ -1,47 +1,27 @@
-let loop, bish, bosh;
-let arr=[];
-const button = document.getElementById("submitButton");
-const results = document.getElementById("result");
+let form = document.getElementById('inputs');
 
-button.addEventListener("click", function() {
-  results.innerHTML="";
-  arr=[];
-  bish = +document.getElementById("bishInput").value;
-  bosh = +document.getElementById("boshInput").value;
-  loop = +document.getElementById("loopInput").value;
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var form = document.getElementById('needs-validation');
-  // Loop over them and prevent submission
-
-  form.addEventListener('submit', function(event) {
-      if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-      }
-      form.classList.add('was-validated');
-      else{
-        bishBosh();
-      }
-
-  })();
-
-  bishBosh();
-  });
-
-  // if(isNaN(parseInt(bish))||isNaN(parseInt(bosh))||isNaN(parseInt(loop))){
-  //   let feedback = document.getElementById("feedback");
-  //   //enabla validate feedback
-  //   //eller alert
-  //   alert("All inputs must be numbers!");
-  //
-  // }
-  //  console.log(bish + bosh + loop);
-
+form.addEventListener('submit', function(event) {
+  if (form.checkValidity() === false) {
+    event.stopPropagation();
+  } else {
+    bishBosh();
+  }
+  form.classList.add('was-validated');
+  event.preventDefault();
+});
 
 function bishBosh() {
+  let bish = +document.getElementById("bishInput").value;
+  let bosh = +document.getElementById("boshInput").value;
+  let loop = +document.getElementById("loopInput").value;
 
-  for (let i = 1; i <=loop; i++) {
+  let resultArr = calculate(bish, bosh, loop);
+  writeOutResult(resultArr);
+}
+
+function calculate(bish, bosh, loop) {
+  let arr = [];
+  for (let i = 1; i <= loop; i++) {
     if (i % bish == 0 && i % bosh == 0) {
       arr.push("Bish-Bosh");
     } else if (i % bish == 0) {
@@ -52,8 +32,15 @@ function bishBosh() {
       arr.push(i);
     }
   }
-  arr.forEach((item, arr) => {
-    results.innerHTML += (item+"<br>"); //ändra från vertikalt?
-  });
+  return arr;
+}
 
+function writeOutResult(arr) {
+  let results = document.getElementById("result");
+  //results.innerHTML = "";
+  let resultString = "";
+  arr.forEach((item, arr) => {
+    resultString += (item + "<br>");
+  });
+  results.innerHTML = resultString;
 }
--- bishBosh2_1.js
+++ bishBosh2_1.js
@@ -1,5 +1,3 @@
-//let loop, bish, bosh;
-//let arr = [];
 let form = document.getElementById('inputs');
 
 form.addEventListener('submit', function(event) {
@@ -7,25 +5,22 @@ form.addEventListener('submit', function(event) {
     event.stopPropagation();
   } else {
     bishBosh();
-  //  calculate();
-    //writeOutResult();
   }
   form.classList.add('was-validated');
   event.preventDefault();
 });
-function bishBosh() {
-  let loop, bish, bosh;
-  bish = +document.getElementById("bishInput").value;
-  bosh = +document.getElementById("boshInput").value;
-  loop = +document.getElementById("loopInput").value;
 
-  let resArr= calculate(bish,bosh,loop);
-  writeOutResult(resArr);
+function bishBosh() {
+  let bish = +document.getElementById("bishInput").value;
+  let bosh = +document.getElementById("boshInput").value;
+  let loop = +document.getElementById("loopInput").value;
+
+  let resultArr = calculate(bish, bosh, loop);
+  writeOutResult(resultArr);
 }
 
-
-function calculate(bish,bosh,loop) {
-  let arr=[];
+function calculate(bish, bosh, loop) {
+  let arr = [];
   for (let i = 1; i <= loop; i++) {
     if (i % bish == 0 && i % bosh == 0) {
       arr.push("Bish-Bosh");
@@ -43,7 +38,7 @@ function calculate(bish,bosh,loop) {
 function writeOutResult(arr) {
   let results = document.getElementById("result");
   //results.innerHTML = "";
-  let resultString="";
+  let resultString = "";
   arr.forEach((item, arr) => {
     resultString += (item + "<br>");
   });
